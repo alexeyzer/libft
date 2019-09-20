4d1807bf5d003dd1494f2114b81728434a3475f4
Alexa Guillermina <aguiller@ga-l6.21-school.ru>
2019-09-20 21:13:20 +0300

    add

---
+++ ft_lstnew.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguiller <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/20 20:00:23 by aguiller          #+#    #+#             */
+/*   Updated: 2019/09/20 20:43:43 by aguiller         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*new_list;
+
+	new_list = NULL;
+	new_list = (t_list*)malloc(sizeof(t_list) * 1);
+	if (new_list == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		new_list->content = NULL;
+		new_list->content = 0;
+	}
+	else
+	{
+		new_list->content = malloc(content_size);
+		if (new_list->content == NULL)
+		{
+			free(new_list);
+			return (NULL);
+		}
+		ft_memcpy(new_list->content, content, content_size);
+		new_list->content_size = content_size;
+	}
+	return (new_list);
+}

ec96357f637d0ee5e66010bfe6ebd2d960d9e0ce
ale <adad>
2019-09-22 13:25:50 +0300

    add

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -3,25 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aguiller <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alexzudin <alexzudin@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 20:09:49 by aguiller          #+#    #+#             */
-/*   Updated: 2019/09/20 12:35:55 by aguiller         ###   ########.fr       */
+/*   Updated: 2019/09/22 13:14:24 by alexzudin        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+#include <stdio.h>
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	int				i;
-	unsigned char	*src1;
-	unsigned char	*src2;
 
 	i = 0;
-	src1 = (unsigned char*)s1;
-	src2 = (unsigned char*)s2;
-	while (src1[i] == src2[i] && src1[i] != '\0' && src2[i] != '\0')
+	while (s1[i] || s2[i])
+	{
+		if(s1[i] > s2[i])
+			return (((unsigned char)(s1[i])) - ((unsigned char)s2[i]));
+		if(s1[i] < s2[i])
+			return (((unsigned char)(s1[i])) - ((unsigned char)s2[i]));
 		i++;
-	return (src1[i] - src2[i]);
+	}
+	return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aguiller <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alexzudin <alexzudin@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 20:09:49 by aguiller          #+#    #+#             */
-/*   Updated: 2019/09/20 14:08:39 by aguiller         ###   ########.fr       */
+/*   Updated: 2019/09/22 13:23:09 by alexzudin        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	int				i;
-	unsigned char	*src1;
-	unsigned char	*src2;
-
+	const unsigned char	*src1;
+	const unsigned char	*src2;
+	
 	i = 0;
 	if (n == 0)
 		return (0);
-	src1 = (unsigned char*)s1;
-	src2 = (unsigned char*)s2;
+	src1 = (const unsigned char*)s1;
+	src2 = (const unsigned char*)s2;
 	while (src1[i] == src2[i] && src1[i] != '\0' && src2[i] != '\0'
 			&& n - 1 > 0)
 	{

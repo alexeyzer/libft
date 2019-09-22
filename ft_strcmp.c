8a20c54a97253cac192e8e1c1003ab9f2e9f500a
Alexa Guillermina <aguiller@ga-g6.21-school.ru>
2019-09-22 13:42:50 +0300

    add

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
+
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	int				i;
@@ -19,9 +19,9 @@ int	ft_strcmp(const char *s1, const char *s2)
 	i = 0;
 	while (s1[i] || s2[i])
 	{
-		if(s1[i] > s2[i])
+		if (s1[i] > s2[i])
 			return (((unsigned char)(s1[i])) - ((unsigned char)s2[i]));
-		if(s1[i] < s2[i])
+		if (s1[i] < s2[i])
 			return (((unsigned char)(s1[i])) - ((unsigned char)s2[i]));
 		i++;
 	}
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,18 +6,18 @@
 /*   By: alexzudin <alexzudin@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 20:09:49 by aguiller          #+#    #+#             */
-/*   Updated: 2019/09/22 13:23:09 by alexzudin        ###   ########.fr       */
+/*   Updated: 2019/09/22 13:41:09 by aguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int				i;
+	int					i;
 	const unsigned char	*src1;
 	const unsigned char	*src2;
-	
+
 	i = 0;
 	if (n == 0)
 		return (0);
